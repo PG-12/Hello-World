a7a107437c3e0b2c7b488aabe1556d14a3f88a4e
PG-12 <79004509+PG-12@users.noreply.github.com>
2021-02-13 22:28:02 +0800

    Create the author of first project in github.sh

---
+++ the author of first project in github.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+${GREEN}[${WHITE}-${GREEN}]${CYAN}  HI I WILL MAKE A FIRST PROJECT NAME PHISH THIS IS THE LOGO
+
+${ORANGE}
+${ORANGE}######  #    #  ##  #######  #    #
+${ORANGE}#    #  #    #  ##  #        #    #
+${ORANGE}#    #  #    #  ##  #        #    #
+${ORANGE}######  ######  ##  #######  ######
+${ORANGE}#       #    #  ##        #  #    #
+${ORANGE}#       #    #  ##        #  #    #
+${ORANGE}#       #    #  ##  #######  #    #
+
+{GREEN}[${WHITE}-${GREEN}]${CYAN}  MY NAME IS PRINCE GUTIERREZ THIS IS THE LOGO
+
+${ORANGE}#####  #####  #  ##    #    ###  ####
+${ORANGE}#   #  #   #  #  # #   #   ##    #
+${ORANGE}#####  #####  #  #  #  #  ##     ####
+${ORANGE}#      #  #   #  #   # #   ##    #
+${ORANGE}#      #   #  #  #    ##    ###  ####
+
